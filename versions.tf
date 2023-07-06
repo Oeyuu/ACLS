730d0d2fec20deb95db98fb3649725a12c271cab
EC2 Default User <ec2-user@ip-10-204-1-28.eu-central-1.compute.internal>
2023-07-06 10:06:45 +0000

    changed code

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "teclify-sandbox-143805577160-terraform-state"
+    key    = "msk/acls/terraform.tfstate"
+    region = "eu-central-1"
+  }
+
   required_providers {
     kafka = {
         source = "mongey/kafka"
