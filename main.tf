778fda0ac74b1496c28fea72dd82fac1bbb608d3
Oeyuu <yu.can.oez@gmail.com>
2023-07-06 11:19:37 +0200

    restructured code

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "kafka" {
+  bootstrap_servers = var.bootstrap_servers
+  sasl_username     = var.super_user_sasl_username
+  sasl_password     = var.super_user_sasl_password
+  sasl_mechanism    = "scram-sha512"
+  tls_enabled       = true
+  skip_tls_verify   = false
+}
